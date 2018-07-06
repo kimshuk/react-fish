b9761d85e369a1921afec9640141cee8a4924b2e
Andrew Kim <jeesoo2002@gmail.com>
2018-07-05 21:27:40 -0700

    finished up to otfrt

---
+++ src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+export class StorePicker extends React.Component {
+  render() {
+    return (
+      <form action="" className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
+  }
+}
+
+// export default StorePicker;
