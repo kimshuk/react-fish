ce71f1f4164cb082dfbc083eb19af78d45f19b41
Andrew Kim <jeesoo2002@gmail.com>
2018-07-17 21:55:18 -0700

    adding remove order func

--- src/components/App.js
+++ src/components/App.js
@@ -50,13 +50,34 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = key => {
+    // 1. take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. update the state
+    fishes[key] = null;
+    // 3. update state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
 
   addToOrder = key => {
+    // 1. take a copy of state
     const order = { ...this.state.order };
+    // 2, Either add to the order, update the number in our order
     order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
+  removeFromOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // remove that item from order
+    delete order[key];
+    // 3. Call setState to update our state object
     this.setState({ order });
   };
 
@@ -76,10 +97,15 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
